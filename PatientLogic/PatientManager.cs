22a9c87b1fbac8d3c8d5a569ca1a497a2494e0f2
Andrea_Nina <alvaradoandrea456@gmail.com>
2022-04-27 01:22:43 -0400

    P2-001 patient manager changed

--- PatientLogic/PatientManager.cs
+++ PatientLogic/PatientManager.cs
@@ -1,8 +1,43 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace PatientLogic
 {
-    public class Class1
+    public class PatientManager
     {
+        private List<Patient> patient;
+
+        public PatientManager()
+        {
+            patient = new List<Patient>();
+        }
+
+        public List<Patient> getPatients()
+        {
+            return patient;
+        }
+        public Patient createPatient(string name, string lastname,int CI, string typeBlood)
+        {
+            Patient paciente = new Patient() { Name = name, LastName = lastname, CI = CI, TypeBlood = typeBlood };
+            patient.Add(paciente);
+            return paciente;
+        }
+        public void updatePatient(string name, string lastname,int ci)
+        {
+            patient.ForEach(p =>
+            {
+                if (p.CI == ci)
+                    p.Name = name; p.LastName = lastname;
+            });
+        }
+        public void removePatient(string name, string lastname, int ci)
+        {
+            patient.ForEach(p =>
+            {
+                if (p.CI == ci)
+                    patient.Remove(p);
+            });
+        }
     }
+    
+    
 }
